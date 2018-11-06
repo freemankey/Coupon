71f25b417a7e506749f557d4f39c4eb67151dddd
freemankey <44804286+freemankey@users.noreply.github.com>
2018-11-06 20:43:28 +0900

    gamen seni

---
+++ app/src/main/java/com/websarva/wings/android/a11/MainActivity.java
@@ -0,0 +1,31 @@
+package com.websarva.wings.android.a11;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        onHomeButtonClick(null);
+    }
+
+    public void onHomeButtonClick(View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onMenuButtonClick(View view) {
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void onCouponButtonClick(View view) {
+        setContentView(R.layout.activity_coupon);
+    }
+
+}
